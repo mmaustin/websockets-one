9e24cfe2216ee6bcbe958c660cb60bcf6714db30
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2024-07-19 16:42:49 -0400

    Set up socket server and connect server to the client

---
+++ public/scripts.js
@@ -0,0 +1,8 @@
+
+
+const socket = io('http://localhost:3000');
+
+socket.on('welcome', data => {
+  console.log(data);
+  socket.emit('thank you', [4, 5, 6])
+});
--- server.js
+++ server.js
@@ -1,5 +1,21 @@
 import express from 'express';
+import { Server } from 'socket.io';
+
 const app = express();
 
 app.use(express.static('public'));
-app.listen(4000);
+const expressServer = app.listen(3000);
+
+//creates a socket.io server
+const io = new Server(expressServer, {
+
+});
+
+io.on('connect', socket => {
+  //console.log(socket.id);
+  socket.emit('welcome', [1, 2, 3]);
+
+  socket.on('thank you', data => {
+    console.log('message from client', data);
+  })
+});
